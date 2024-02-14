0b1f84c601bf8c60c8a6ee0b667b1fe1baed53ef
Bojan Pavlovic <bojan@pitchstonetechnology.com>
2024-02-14 12:39:01 +0100

    fix add save in localstorage

--- bojan/src/app/task/task.component.ts
+++ bojan/src/app/task/task.component.ts
@@ -27,4 +27,21 @@ export class TaskComponent {
     localStorage.setItem('users', JSON.stringify(this.allUsers))
   }
 
+  ngOnInit(): void {
+    this.retrieveDataFromLocalStorage();
+  }
+
+  retrieveDataFromLocalStorage(): void {
+    const storedData = localStorage.getItem('users');
+    if (storedData) {
+      try {
+        this.allUsers = JSON.parse(storedData);
+      } catch (error) {
+        console.error('Error parsing data from local storage:', error);
+      }
+    } else {
+      console.warn('No data found in local storage.');
+    }
+  }
+
 }
